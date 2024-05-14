d4dbc7f18c4d0ea6cc56ab6b4924659e0be22b95
zpj80231 <zpj80231@163.com>
2024-05-14 17:55:17 +0800

    feat(snail-module): 添加模块

---
+++ snail-module/snail-module-admin/src/main/java/com/snail/module/admin/AdminApplication.java
@@ -0,0 +1,17 @@
+package com.snail.module.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangpengjun
+ * @date 2024/5/13
+ */
+@SpringBootApplication
+public class AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+
+}
