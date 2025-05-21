33624bb8c7923dc8532a71ae7a4b6046cb21e6a6
zpj80231 <zpj80231@163.com>
2025-05-21 15:09:37 +0800

    feat(source-rpc): 更新rpc服务端，支持多服务器端启动，模拟多个serviceImpl在多个服务端上，用于客户端的自动负载均衡测试

--- snail-source-code/source-rpc/src/main/java/com/sanil/source/code/rpc/RpcServer.java
+++ snail-source-code/source-rpc/src/main/java/com/sanil/source/code/rpc/RpcServer.java
@@ -12,12 +12,7 @@ public class RpcServer {
 
     public static void main(String[] args) {
         // 启动一个服务端，默认端口为 8023
-        // new RpcServerManager().start();
-
-        // 启动多个服务端，模拟serviceImpl在多个服务器上，客户端自动负载均衡
-        new Thread(() -> new RpcServerManager().start()).start();
-        new Thread(() -> new RpcServerManager(8024).start()).start();
-        new Thread(() -> new RpcServerManager(8025).start()).start();
+        new RpcServerManager().start();
     }
 
 }
