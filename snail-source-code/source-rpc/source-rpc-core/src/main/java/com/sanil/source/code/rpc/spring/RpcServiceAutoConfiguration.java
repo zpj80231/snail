3d1beba35dd9234a11429bd5493a8a99390e3203
zpj80231 <zpj80231@163.com>
2025-07-10 10:04:17 +0800

    fix(source-rpc): 修复在只使用了@EnableRpcClient时，可能导致RpcServiceAutoConfiguration也自动加载的问题

--- snail-source-code/source-rpc/source-rpc-core/src/main/java/com/sanil/source/code/rpc/spring/RpcServiceAutoConfiguration.java
+++ snail-source-code/source-rpc/source-rpc-core/src/main/java/com/sanil/source/code/rpc/spring/RpcServiceAutoConfiguration.java
@@ -2,7 +2,6 @@ package com.sanil.source.code.rpc.spring;
 
 import com.sanil.source.code.rpc.server.RpcServerManager;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * RPC 服务自动配置
@@ -10,7 +9,6 @@ import org.springframework.context.annotation.Configuration;
  * @author zhangpj
  * @date 2025/07/09
  */
-@Configuration
 public class RpcServiceAutoConfiguration {
 
     @Bean(initMethod = "startAsync", destroyMethod = "destroy")
